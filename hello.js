aa96b6fd0eba6096836b611fcd8cf2af8ddda8d4
zodern <zodern@users.noreply.github.com>
2015-05-20 14:04:21 -0500

    hero text also scales on scroll

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
 var divs = $('.hero h1');
 $(window).scroll(function(){
   var opacity = 1 - ($(window).scrollTop()/(window.innerHeight/2));
+  var scale = ($(window).scrollTop() / window.innerHeight) + 1; 
   $(divs).css('opacity', opacity);
+  $(divs).css('transform', 'scale(' + scale + ')');
 });
