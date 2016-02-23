68d1580326343914d128197ecdf40153c905e730
zodern <zodern@users.noreply.github.com>
2016-02-23 10:07:31 -0600

    forgot to push

--- hello.js
+++ hello.js
@@ -1,5 +1,8 @@
 var divs = $('.hero h1');
 $(window).scroll(function(){
+  if($(window).scrollTop() > window.innerHeight) {
+    return;
+  }
   var opacity = 1 - ($(window).scrollTop()/(window.innerHeight/2));
   var scale = ($(window).scrollTop() / window.innerHeight) + 1; 
   $(divs).css('opacity', opacity);
